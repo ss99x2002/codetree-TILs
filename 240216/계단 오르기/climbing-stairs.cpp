c4308deeb22ef78220088bbf85270119852a50d3
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2024-02-16 01:06:22 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240216/계단 오르기/climbing-stairs.cpp
+++ 240216/계단 오르기/climbing-stairs.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int arr[1001];
+long long arr[1001];
 
 int main() {
     int n;
@@ -10,10 +10,12 @@ int main() {
     arr[1] = 0;
     arr[2] = 1;
     arr[3] = 1;
+    arr[4] = 1;
 
-    for (int i=4; i<=n; i++){
-        arr[i] = arr[i-2]+ arr[i-3];
+    for (int i = 5; i <= n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 3];
+        cout << "현재 i값은? " << i << "배열 안 값은" << arr[i] << "\n";
     }
-    cout << arr[n]%10007;
+    cout << arr[n] % 10007;
     return 0;
 }
