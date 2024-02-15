fa790772b4ff4700629ddec9b200ab7b2831d9fe
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2024-02-16 01:46:50 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240216/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+
+int main() {
+
+    int n;
+    cin >> n;
+    arr[1] = 1;
+    arr[2] = 2;
+    arr[3] = 3;
+
+    for (int i=4; i<=n; i++){
+        arr[i] = (arr[i-1]+arr[i-2])%10007;
+    }
+
+    cout << arr[n];
+
+    return 0;
+}
