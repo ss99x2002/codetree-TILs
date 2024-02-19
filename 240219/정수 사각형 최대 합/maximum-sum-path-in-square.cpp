2b69712b8259993e2ae7f2ed4093a1e152460ed1
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2024-02-19 23:02:55 +0900

    Title: 정수 사각형 최대 합, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240219/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 240219/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -8,31 +8,37 @@ int main() {
 
     int input;
 
-    for (int i=0; i<N; i++){
-        for (int j=0; j<N; j++){
-            cin >>input;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> input;
             dp[i][j] = input;
         }
     }
 
-    for (int i=1; i<N; i++){
-        dp[i][0] = dp[i-1][0] + dp[i][0];
+    for (int i = 1; i < N; i++) {
+        dp[i][0] = dp[i - 1][0] + dp[i][0];
     }
 
-    for (int i=1; i<N; i++){
-        dp[i][i] = dp[i-1][i-1] + dp[i][i];
+    for (int i = 1; i < N; i++) {
+        dp[0][i] = dp[0][i - 1] + dp[0][i];
     }
 
-    for (int i=2; i<N; i++){
-        for (int j=1; j<N; j++){
-            dp[i][j] = max(dp[i-1][j]+dp[i][j] , dp[i-1][j-1]+dp[i][j]);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+        }
+    }
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < N; j++) {
+            dp[i][j] = max(dp[i - 1][j] + dp[i][j], dp[i][j - 1] + dp[i][j]);
         }
     }
 
     int ans = 0;
 
-    for (int i=0; i<N; i++){
-        ans = max(ans, dp[N-1][i]);
+    for (int i = 0; i < N; i++) {
+        ans = max(ans, dp[N - 1][i]);
     }
 
     cout << ans;
