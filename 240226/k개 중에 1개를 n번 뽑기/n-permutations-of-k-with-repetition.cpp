c85ac2ca3568bc427f11f1fad6e6b3937e701204
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2024-02-26 22:03:00 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240226/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int K, N;
+int arr[9];
+bool visited[9];
+
+void func(int k) {
+    if (k == N) {
+        for (int i=0; i<N; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i=1; i<=N; i++){
+        arr[k] = i;
+        visited[i] = true;
+        func(k+1);
+        visited[i] = false;
+    }
+}
+
+
+
+int main() {
+
+    cin >> K >> N;
+    // K까지의 수 N개 고르는 순열 만들기 
+    func(0);
+    return 0;
+}
