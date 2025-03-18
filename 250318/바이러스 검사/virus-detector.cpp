8fd86b47978359a74a73288edb10200e166704e7
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-03-18 14:34:20 +0900

    Title: 바이러스 검사, Time: 355ms, Memory: 8MB, Status: Passed - CodeTree

--- 250318/바이러스 검사/virus-detector.cpp
+++ 250318/바이러스 검사/virus-detector.cpp
@@ -2,13 +2,14 @@
 #include <cmath>
 using namespace std;
 
-int n; // n개의 식당 
-float customer[1000001]; // 식당에 있는 고객 수
+long long n; // n개의 식당 
+long long customer[1000001]; // 식당에 있는 고객 수
 float maxL, maxM; // 팀장 검사 최대 고객 수, 팀원 검사 최대 고객 수
 long long ans =0;
 
-long long calNum(float memberC){
-    return (ceil(memberC/maxM));
+long long calNum(long long memberC){
+    if(memberC< 0) return 0;
+    else return (ceil(memberC/maxM));
 }
 
 // 출력 : n개의 식당 고객들 모두 검사하기 위한 검사자의 최소 수 
@@ -23,6 +24,5 @@ int main() {
         ans ++;
         ans = ans + calNum(customer[i]-maxL);
     }
-
     cout << ans;
 }
