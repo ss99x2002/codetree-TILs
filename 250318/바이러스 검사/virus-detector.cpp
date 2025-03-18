68dc82711a0229b4e4a3c3cbc1f5d73de5ba5216
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-03-18 14:30:44 +0900

    Title: 바이러스 검사, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250318/바이러스 검사/virus-detector.cpp
+++ 250318/바이러스 검사/virus-detector.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int n; // n개의 식당 
-int customer; // 식당에 있는 고객 수
-int maxL, maxM; // 팀장 검사 최대 고객 수, 팀원 검사 최대 고객 수
-long long maxCustomer = 0;
-int ans =0;
+float customer[1000001]; // 식당에 있는 고객 수
+float maxL, maxM; // 팀장 검사 최대 고객 수, 팀원 검사 최대 고객 수
+long long ans =0;
 
-long long calNum(int maxC){
-    if (maxC - maxL <= 0){
-        return 1;
-    }
-    else {
-        if ((maxC-maxL)%maxM == 0 || (maxC-maxL)/maxM<0){
-            return n*((maxC-maxL)/maxM + 1);
-        }
-        else{
-            return n*((maxC-maxL)/maxM + 2);
-        }
-    }
+long long calNum(float memberC){
+    return (ceil(memberC/maxM));
 }
-
 
 // 출력 : n개의 식당 고객들 모두 검사하기 위한 검사자의 최소 수 
 int main() {
     cin >> n;
     for (int i=0; i<n; i++){
-        cin >> customer;
-        if (maxCustomer < customer) {
-            maxCustomer = customer;
-        }
+        cin >> customer[i];
     }
     cin >> maxL >> maxM;
-    cout << calNum(maxCustomer);
-    return 0;
+
+    for (int i=0; i<n; i++){
+        ans ++;
+        ans = ans + calNum(customer[i]-maxL);
+    }
+
+    cout << ans;
 }
