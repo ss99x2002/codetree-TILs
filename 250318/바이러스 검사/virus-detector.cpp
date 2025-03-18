6bcacd5f3e67743fc3070b020909d69d474f7f6c
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-03-18 14:14:49 +0900

    Title: 바이러스 검사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250318/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int n; // n개의 식당 
+int customer; // 식당에 있는 고객 수
+int maxL, maxM; // 팀장 검사 최대 고객 수, 팀원 검사 최대 고객 수
+long long maxCustomer = 0;
+int ans =0;
+
+long long calNum(int maxC){
+    if (maxC - maxL <= 0){
+        return 1;
+    }
+    else {
+        if ((maxC-maxL)%maxM == 0){
+            return n*((maxC-maxL)/maxM + 1);
+        }
+        else{
+            return n*((maxC-maxL)/maxM + 2);
+        }
+    }
+}
+
+
+// 출력 : n개의 식당 고객들 모두 검사하기 위한 검사자의 최소 수 
+int main() {
+    cin >> n;
+    for (int i=0; i<n; i++){
+        cin >> customer;
+        if (maxCustomer < customer) {
+            maxCustomer = customer;
+        }
+    }
+    cin >> maxL >> maxM;
+    cout << calNum(maxCustomer);
+    return 0;
+}
