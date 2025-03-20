ca2947278568885163361c3b20b3293011b41d8d
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-03-20 15:08:24 +0900

    Title: 2048 게임, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 250320/2048 게임/2048-game.cpp
+++ 250320/2048 게임/2048-game.cpp
@@ -1,61 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int n;
 int board[21][21];
-int tmp[21][21];
-bool isSum[21][21]; // 합쳐진 여부 확인 (연쇄 합침 방지)
 int maxAns = 0;
 
-// 상 우 하 좌
-int dx[4] = {-1,0,1,0}; 
-int dy[4] = {0,1,0,-1};
-
-void copyTmp() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            tmp[i][j] = board[i][j];
-        }
-    }
-}
-
-void initIsSum() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            isSum[i][j] = false;
-        }
-    }
-}
-
-void returnBoard() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            board[i][j] = tmp[i][j];
-        }
-    }
-}
+// 이동 방향 (상, 우, 하, 좌)
+int dx[4] = {-1, 0, 1, 0}; 
+int dy[4] = {0, 1, 0, -1};
 
 // 보드 이동 로직
 void moveBoard(int dir) {
-    dir = dir % 4;
-    initIsSum();
-
+    vector<vector<bool>> isSum(n, vector<bool>(n, false)); // 합친 여부 체크
     if (dir == 0) { // 위로 이동
         for (int j = 0; j < n; j++) {
             for (int i = 1; i < n; i++) {
                 if (board[i][j] == 0) continue;
                 int x = i;
-                
-                while (x > 0 && board[x-1][j] == 0) {
-                    board[x-1][j] = board[x][j];
+                while (x > 0 && board[x - 1][j] == 0) { // 빈 칸이면 이동
+                    board[x - 1][j] = board[x][j];
                     board[x][j] = 0;
                     x--;
                 }
-
-                if (x > 0 && board[x-1][j] == board[x][j] && !isSum[x-1][j]) {
-                    board[x-1][j] *= 2;
+                if (x > 0 && board[x - 1][j] == board[x][j] && !isSum[x - 1][j]) { // 합칠 수 있는 경우
+                    board[x - 1][j] *= 2;
                     board[x][j] = 0;
-                    isSum[x-1][j] = true;
+                    isSum[x - 1][j] = true;
                 }
             }
         }
@@ -64,17 +37,15 @@ void moveBoard(int dir) {
             for (int j = n - 2; j >= 0; j--) {
                 if (board[i][j] == 0) continue;
                 int y = j;
-
-                while (y < n - 1 && board[i][y+1] == 0) {
-                   board[i][y+1] = board[i][y];
+                while (y < n - 1 && board[i][y + 1] == 0) { // 빈 칸이면 이동
+                    board[i][y + 1] = board[i][y];
                     board[i][y] = 0;
                     y++;
                 }
-
-                if (y < n - 1 && board[i][y+1] == board[i][y] && !isSum[i][y+1]) {
-                    board[i][y+1] *= 2;
+                if (y < n - 1 && board[i][y + 1] == board[i][y] && !isSum[i][y + 1]) { // 합칠 수 있는 경우
+                    board[i][y + 1] *= 2;
                     board[i][y] = 0;
-                    isSum[i][y+1] = true;
+                    isSum[i][y + 1] = true;
                 }
             }
         }
@@ -83,17 +54,15 @@ void moveBoard(int dir) {
             for (int i = n - 2; i >= 0; i--) {
                 if (board[i][j] == 0) continue;
                 int x = i;
-
-                while (x < n - 1 && board[x+1][j] == 0) {
-                    board[x+1][j] = board[x][j];
+                while (x < n - 1 && board[x + 1][j] == 0) { // 빈 칸이면 이동
+                    board[x + 1][j] = board[x][j];
                     board[x][j] = 0;
                     x++;
                 }
-
-                if (x < n - 1 && board[x+1][j] == board[x][j] && !isSum[x+1][j]) {
-                    board[x+1][j] *= 2;
+                if (x < n - 1 && board[x + 1][j] == board[x][j] && !isSum[x + 1][j]) { // 합칠 수 있는 경우
+                    board[x + 1][j] *= 2;
                     board[x][j] = 0;
-                    isSum[x+1][j] = true;
+                    isSum[x + 1][j] = true;
                 }
             }
         }
@@ -102,25 +71,24 @@ void moveBoard(int dir) {
             for (int j = 1; j < n; j++) {
                 if (board[i][j] == 0) continue;
                 int y = j;
-
-                while (y > 0 && board[i][y-1] == 0) {
-                  board[i][y-1] = board[i][y];
-                  board[i][y] = 0;
+                while (y > 0 && board[i][y - 1] == 0) { // 빈 칸이면 이동
+                    board[i][y - 1] = board[i][y];
+                    board[i][y] = 0;
                     y--;
                 }
-
-                if (y > 0 && board[i][y-1] == board[i][y] && !isSum[i][y-1]) {
-                    board[i][y-1] *= 2;
+                if (y > 0 && board[i][y - 1] == board[i][y] && !isSum[i][y - 1]) { // 합칠 수 있는 경우
+                    board[i][y - 1] *= 2;
                     board[i][y] = 0;
-                    isSum[i][y-1] = true;
+                    isSum[i][y - 1] = true;
                 }
             }
         }
     }
 }
 
+// 백트래킹으로 5번 이동하며 최대값 탐색
 void backTracking(int cnt) {
-    if (cnt == 5) { // 5번 움직인 후 최댓값 찾기
+    if (cnt == 5) { // 5번 이동 후 최대값 저장
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 maxAns = max(maxAns, board[i][j]);
@@ -128,12 +96,20 @@ void backTracking(int cnt) {
         }
         return;
     }
-    
-    copyTmp();
+
+    int tempBoard[21][21]; // 현재 상태 저장
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            tempBoard[i][j] = board[i][j];
+
     for (int dir = 0; dir < 4; dir++) {
         moveBoard(dir);
         backTracking(cnt + 1);
-        returnBoard();
+
+        // 이전 상태로 복구
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = tempBoard[i][j];
     }
 }
 
