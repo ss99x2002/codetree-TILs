772e08629c436d5cf3d867556d4993e2f488a344
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-04-01 14:30:16 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250401/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
+++ 250401/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int n, t, p;
-int maxSum = 0;
-vector<pair<int, int>> tasks;  // {소요시간, 수익}
-vector<int> dp;  // DP 배열
-
-// 백트래킹 함수
-void backTracking(int idx, int sum) {
-    // 최대 수익 갱신
-    maxSum = max(maxSum, sum);
-
-    // 가능한 다음 작업을 탐색
-    for (int i = idx + 1; i < n; i++) {
-        // 현재 작업이 끝난 시간 <= 다음 작업이 시작하는 시간일 경우
-        if (tasks[idx].first + idx <= i) {
-            backTracking(i, sum + tasks[i].second);  // 선택된 작업을 추가
-        }
-    }
-}
-
 int main() {
+    int n;
     cin >> n;
 
-    // 작업 정보 입력
+    vector<int> dp(n + 1, 0);
+    vector<pair<int, int>> arr(n);
+
     for (int i = 0; i < n; i++) {
-        cin >> t >> p;
-        tasks.push_back({t, p}); // 소요 시간과 수익
+        cin >> arr[i].first >> arr[i].second; // {기간, 수익}
     }
 
-    // 모든 작업을 시작점으로 해서 backtracking 호출
-    for (int i = 0; i < n; i++) {
-        backTracking(i, tasks[i].second);  // 현재 작업부터 시작
+    for (int i = n - 1; i >= 0; i--) {
+        int time = arr[i].first, profit = arr[i].second;
+
+        if (i + time <= n) {
+            dp[i] = max(dp[i + time] + profit, dp[i + 1]);
+        } else {
+            dp[i] = dp[i + 1];
+        }
     }
 
-    cout << maxSum << "\n";  // 최대 수익 출력
+    cout << dp[0] << "\n";
     return 0;
 }
+
+   // 내가 구현해야 하는 것
+    // 어떤것이든 첫번째로 선택 가능.
+    // 그 이후로는 조건이 -> 이전에 선택했던 것의 끝나는 시간 <= 선택하는 것의 시작 시간
+    // 이 조건이 맞다면 backTraking 계속 진행 함.
+    // 선택한 것의 index가 n에 도달했다면 -> 포인트의 합을 max인지 아닌지 판단 후 갱신 필요.
