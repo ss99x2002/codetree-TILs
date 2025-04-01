cb93854447ef27db23560c6838118c0efc3a7afc
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-04-01 14:42:20 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250401/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
+++ 250401/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+int n,t,p;
+int task[16];
+int maxSum = 0;
 
-    vector<int> dp(n + 1, 0);
-    vector<pair<int, int>> arr(n);
+// 외주작업에 걸리는 시간 t, 수익 p 
+// work = {idx(시작 시간),p} 담고있음. 
+// time = {시작시간, 끝나는 시간} 담도록. 
 
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i].first >> arr[i].second; // {기간, 수익}
+vector<pair<int,int>> work;
+vector<pair<int,int>> times;
+
+void backTraking(int idx, int sum){
+    if (times[idx].first > n) {
+        return; 
     }
-
-    for (int i = n - 1; i >= 0; i--) {
-        int time = arr[i].first, profit = arr[i].second;
-
-        if (i + time <= n) {
-            dp[i] = max(dp[i + time] + profit, dp[i + 1]);
-        } else {
-            dp[i] = dp[i + 1];
+    for (int i= idx+1; i<n; i++){
+        if (times[idx].second <= times[i].first && times[i].second <=n){
+            backTraking(i, sum+work[i].second); 
         }
     }
+    
+    maxSum = max(maxSum,sum);
+}
 
-    cout << dp[0] << "\n";
+int main() {
+    cin >> n;
+    for (int i=0; i<n; i++){
+        cin >> t >> p;
+        work.push_back({i,p}); //시작 시간(이자 idx 역할도 됨.),point
+        times.push_back({i, i+t}); // 시작시간, 끝나는 시간
+    }
+
+    for (int i=0; i<n; i++){
+        backTraking(i,work[i].second);
+    }
+
+    cout << maxSum << "\n";
     return 0;
 }
 
