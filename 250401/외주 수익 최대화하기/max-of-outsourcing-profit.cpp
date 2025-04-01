0051b0261d74dc6315bb9309d2f632eded9d2fec
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-04-01 14:12:15 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250401/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,t,p;
+int task[16];
+int maxSum = 0;
+
+// 외주작업에 걸리는 시간 t, 수익 p 
+// work = {idx(시작 시간),p} 담고있음. 
+// time = {시작시간, 끝나는 시간} 담도록. 
+
+vector<pair<int,int>> work;
+vector<pair<int,int>> times;
+
+int backTraking(int idx, int sum){
+    maxSum = max(maxSum,sum);
+    for (int i= idx+1; i<n; i++){
+        if (times[idx].second <= times[i].first){
+            backTraking(i, sum+work[i].second); 
+        }
+    }
+    return maxSum;
+}
+
+int main() {
+    cin >> n;
+    for (int i=0; i<n; i++){
+        cin >> t >> p;
+        work.push_back({i,p}); //시작 시간(이자 idx 역할도 됨.),point
+        times.push_back({i, i+t}); // 시작시간, 끝나는 시간
+    }
+
+    for (int i=0; i<n; i++){
+        if (times[i].second <= n){
+            backTraking(i,work[i].second);
+        }
+    }
+
+    cout << maxSum << "\n";
+    return 0;
+}
+
+   // 내가 구현해야 하는 것
+    // 어떤것이든 첫번째로 선택 가능.
+    // 그 이후로는 조건이 -> 이전에 선택했던 것의 끝나는 시간 <= 선택하는 것의 시작 시간
+    // 이 조건이 맞다면 backTraking 계속 진행 함.
+    // 선택한 것의 index가 n에 도달했다면 -> 포인트의 합을 max인지 아닌지 판단 후 갱신 필요.
