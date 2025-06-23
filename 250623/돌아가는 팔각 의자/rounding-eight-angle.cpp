5d3124500e0db6399fb875a441c7989dde478afb
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-06-23 23:03:36 +0900

    Title: 돌아가는 팔각 의자, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250623/돌아가는 팔각 의자/rounding-eight-angle.cpp
+++ 250623/돌아가는 팔각 의자/rounding-eight-angle.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 // 북부 N, 남부 S
@@ -15,21 +17,143 @@ using namespace std;
 // 각 의자의 12시 방향에 앉아있던 남쪽 지방 사람의 착석 여부 알아내기
 
 
-int s1[8];
+int s1[8]; // 0: 북쪽지방, 1:남쪽지방 의미.
 int s2[8];
 int s3[8];
 int s4[8];
+string s;
+int k;
+int changeNum;
+int d; // 1: 시계방향, -1: 반시계방향
+int used[5] = {0};
+
+
+void moveChair(int num,int dir){
+    if (num == 1) {
+        int tmp = s1[0];
+        if (dir == -1) {
+            for (int i=1; i<8; i++){
+                s1[i-1] = s1[i];
+            }
+           s1[7] = tmp;
+        }
+        else {
+            int tmp = s1[7];
+            for (int i=0; i<7; i++){
+                s1[i+1] = s1[i];
+            }
+           s1[0] = tmp;
+        }
+    }
+
+    else if (num == 2) {
+        int tmp = s2[0];
+        if (dir == -1) {
+            for (int i=1; i<8; i++){
+                s2[i-1] = s2[i];
+            }
+           s2[7] = tmp;
+        }
+        else {
+            int tmp = s2[7];
+            for (int i=0; i<7; i++){
+                s2[i+1] = s2[i];
+            }
+           s2[0] = tmp;
+        }
+
+    }
+
+    else if (num == 3) {
+         int tmp = s3[0];
+        if (dir == -1) {
+            for (int i=1; i<8; i++){
+                s3[i-1] = s3[i];
+            }
+           s3[7] = tmp;
+        }
+        else {
+            int tmp = s3[7];
+            for (int i=0; i<7; i++){
+                s3[i+1] = s3[i];
+            }
+           s3[0] = tmp;
+        }
+
+    }
+    else if (num == 4){
+        int tmp = s4[0];
+        if (dir == -1) {
+            for (int i=1; i<8; i++){
+                s4[i-1] = s4[i];
+            }
+           s4[7] = tmp;
+        }
+        else {
+            int tmp = s4[7];
+            for (int i=0; i<7; i++){
+                s4[i+1] = s4[i];
+            }
+           s4[0] = tmp;
+        }
+
+    }
+}
+
+void checkMove(int num, int dir){
+
+if (used[num] != 0) return; // 이미 처리된 의자면 무시
+    
+    used[num] = dir;
+
+    if (num == 1) {
+        if (s1[2] != s2[6]) checkMove(2, -dir);
+    }
+    else if (num == 2) {
+        if (s1[2] != s2[6]) checkMove(1, -dir);
+        if (s2[2] != s3[6]) checkMove(3, -dir);
+    }
+    else if (num == 3) {
+        if (s3[6] != s2[2]) checkMove(2, -dir);
+        if (s3[2] != s4[6]) checkMove(4, -dir);
+    }
+    else if (num == 4) {
+        if (s4[6] != s3[2]) checkMove(3, -dir);
+    }
+}
+
+void initUsed(){
+    for (int i=1; i<=4; i++){
+        used[i] = 0;
+    }
+}
+
 
 int main() {
-    for (int i=0; i<8; i++){
-        cin >> s1[i];
-    }
-        for (int i=0; i<8; i++){
-        cin >> s1[i];
-    }
-        for (int i=0; i<8; i++){
-        cin >> s1[i];
-    }
+    cin >> s;
+for (int i = 0; i < 8; i++) s1[i] = s[i] - '0';
 
+cin >> s;
+for (int i = 0; i < 8; i++) s2[i] = s[i] - '0';
+
+cin >> s;
+for (int i = 0; i < 8; i++) s3[i] = s[i] - '0';
+
+cin >> s;
+for (int i = 0; i < 8; i++) s4[i] = s[i] - '0';
+    cin >> k;
+
+    for(int i=0; i<k; i++){
+        cin >> changeNum >> d;
+        initUsed();
+        checkMove(changeNum,d);
+        for (int i=1; i<=4; i++){
+        if (used[i] != 0){
+            moveChair(i,used[i]);
+        }
+    }
+}
+
+    cout << (s1[0] + (2*s2[0]) + (4*s3[0]) + (8*s4[0]));
     return 0;
 }
